f31856861ac21e0e6461fbe55e775903d942fe26
Gareth Johns <info@garethjohnsdesign.com>
2017-06-02 23:19:52 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).foundation();
 
-// $('body').removeClass('fade-out');
+
 
 $('.menu__sub-navigation a').smoothScroll({
   offset: -32,
@@ -13,3 +13,12 @@ $(function() {
     animateClass: 'is-animating'
   }).init();
 })
+
+$('.carousel').flickity({
+cellAlign: 'left',
+wrapAround: true,
+pageDots: false,
+imagesLoaded: true,
+percentPosition: false,
+arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
+});
