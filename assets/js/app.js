0f0bbd65d8eff61f6e80fccebba270421530c167
Gareth Johns <info@garethjohnsdesign.com>
2017-06-04 17:45:02 +0100

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
 $(document).foundation();
 
 
+// 1. Smooth Scroll
+// ----------------
 
 $('.menu__sub-navigation a').smoothScroll({
   offset: -32,
   easing: 'linear',
 });
+
+// 2. Carousel
+// -----------
 
 $('.carousel').flickity({
 cellAlign: 'left',
@@ -16,10 +21,16 @@ percentPosition: false,
 arrowShape: 'M100,46.875H11.9688l17.4688-17.4688L25,25L0,50l25,25l4.4062-4.4062L11.9688,53.125H100V46.875z'
 });
 
-$(function() {
-  var wow = new WOW({
-    boxClass: 'wow',
-    offset:       32, 
-    animateClass: 'is-animating'
-  }).init();
+// 3. Animate on Scroll
+// --------------------
+
+$(window).on('resize', function () { AOS.refresh(); });
+$(window).on('load', function() { setTimeout(AOS.refreshHard, 150); });
+
+$(document).ready(function () {
+  AOS.init({ 
+   startEvent: 'load', 
+   offset: 64,
+   easing: 'ease-in-out-quart', 
+   duration: 600,  once: false });            
 });
